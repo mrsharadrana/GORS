79f10501eebe843bc878e6fa33fd0eb8d0f83a58
Sharad Rana <53858653+mrsharadrana@users.noreply.github.com>
2026-08-30 19:40:58 +0530

    Remove redundant timestamp test

--- tests/test_phase8_timestamp_format.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-
-from GORS_APP_PROD.app import format_snapshot_time
-
-
-def test_format_snapshot_time_uses_ist():
-    value = "2026-08-30T10:49:47"
-    assert format_snapshot_time(value) == "30-Aug-2026 04:19 PM IST"
